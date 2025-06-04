743b10e0c78f70d56ad3a836680eb2bd96ac085c
Visal Raza Zaidi <visalraza@gmail.com>
2025-06-04 23:18:31 +0500

    Refactor Navbar layout for improved structure and styling

--- Portfolio Website for a Freelancer/src/App.tsx
+++ Portfolio Website for a Freelancer/src/App.tsx
@@ -21,24 +21,28 @@ import Contact from "./components/Contact.tsx";
 export default function Component() {
   return (
     <>
-      <Navbar fluid rounded>
-        <NavbarBrand href="#">
-          <img className="mr-20 h-6 sm:h-9" alt="" />
-          <span className="self-center text-xl font-sans whitespace-nowrap dark:text-white">
-            Alex Carter
-          </span>
-        </NavbarBrand>
-        <NavbarToggle />
-        <NavbarCollapse>
-          <NavbarLink href="#home" active>
-            Home
-          </NavbarLink>
-          <NavbarLink href="#about">About</NavbarLink>
-          <NavbarLink href="#skills">Skills</NavbarLink>
-          <NavbarLink href="#projects">Projects</NavbarLink>
-          <NavbarLink href="#services">Services</NavbarLink>
-          <NavbarLink href="#contact">Contact</NavbarLink>
-        </NavbarCollapse>
+      <Navbar fluid rounded className="w-full justify-center">
+        <div className="flex items-center justify-between w-full max-w-screen-xl px-4 py-2">
+          <NavbarBrand href="#" className="flex items-center mx-8">
+            <img className="mr-4 h-6 sm:h-9" alt="" />
+            <span className="self-center text-xl font-sans whitespace-nowrap dark:text-white">
+              Alex Carter
+            </span>
+          </NavbarBrand>
+          <div className="flex-1 flex justify-center">
+            <NavbarToggle />
+            <NavbarCollapse className="flex flex-row items-center justify-center gap-8">
+              <NavbarLink href="#home" active>
+                Home
+              </NavbarLink>
+              <NavbarLink href="#about">About</NavbarLink>
+              <NavbarLink href="#skills">Skills</NavbarLink>
+              <NavbarLink href="#projects">Projects</NavbarLink>
+              <NavbarLink href="#services">Services</NavbarLink>
+              <NavbarLink href="#contact">Contact</NavbarLink>
+            </NavbarCollapse>
+          </div>
+        </div>
       </Navbar>
       <Hero />
       <About />
