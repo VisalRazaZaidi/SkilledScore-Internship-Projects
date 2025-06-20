bd4cca9004d1824365de9c3a08857e8001abc67b
Visal Raza Zaidi <visalraza@gmail.com>
2025-06-20 20:43:15 +0500

    Update firebase.js

--- Build a Blog Platform with Content Management/client/src/firebase.js
+++ Build a Blog Platform with Content Management/client/src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAbYmGNrKjGsxafROlGJ7iO69LwKdU1l2s",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "skilledscore-mern-blog-project.firebaseapp.com",
   projectId: "skilledscore-mern-blog-project",
   storageBucket: "skilledscore-mern-blog-project.firebasestorage.app",
