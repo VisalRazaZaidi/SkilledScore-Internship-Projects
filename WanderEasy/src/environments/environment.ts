5fbe043fc46b2671360a4415148cbcb2e19cf546
Visal Raza Zaidi <visalraza@gmail.com>
2025-07-31 01:16:20 +0500

    fix

--- WanderEasy/src/environments/environment.ts
+++ WanderEasy/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   weatherApiBaseUrl:'https://api.openweathermap.org/data/2.5/weather',
-  openWeatherAPIKey: '4792fa7464a407807c7becc41c817ac5'
+  openWeatherAPIKey: "4792fa7464a407807c7becc41c817ac5",
 };
